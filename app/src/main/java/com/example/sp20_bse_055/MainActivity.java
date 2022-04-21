f9f7a191c95bc0cbd041e529c2fb494dbad98795
Muhammad Umer Mukhtiar <92437033+sp20bse055@users.noreply.github.com>
2022-04-21 13:43:21 +0500

    upload

--- app/src/main/java/com/example/sp20_bse_055/MainActivity.java
+++ app/src/main/java/com/example/sp20_bse_055/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.sp20_bse_055;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+private FragmentManager fragmentManager;
+private leftfrag leftf;
+private FragmentTransaction fragmentTransaction;
+private rightfrag rightf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager=getSupportFragmentManager();
+        fragmentTransaction=fragmentManager.beginTransaction();
+        int orientation = getResources().getConfiguration().orientation;
 
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            leftf = leftfrag.newInstance();
+            rightf = rightfrag.newInstance();
+
+            fragmentTransaction.replace(R.id.leftf, leftf);
+            fragmentTransaction.replace(R.id.rightf,rightf);
+
+            fragmentTransaction.commit();
     }
-}
+}}
